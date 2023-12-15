3118fde7208d716a290e12f389ba5f48785be5ff
Behnam2064 <Behnamgraphic3@gmail.com>
2023-12-15 22:43:54 +0330

    Create a utility library (.Net)

---
+++ DOTNET.Common/Reflections/ReflectionHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOTNET.Common.Reflections
+{
+    public class ReflectionHelper
+    {
+
+        /// <summary>
+        /// https://stackoverflow.com/questions/10261824/how-can-i-get-all-constants-of-a-type-by-reflection
+        /// How can I get all constants of a type by reflection?
+        /// </summary>
+        /// <param name="type">The type in which it is to be searched</param>
+        /// <returns>Constants found</returns>
+        public static List<FieldInfo> GetConstants(System.Type type)
+        => type.GetFields(BindingFlags.Public | BindingFlags.Static |
+                  BindingFlags.FlattenHierarchy)
+                    .Where(fi => fi.IsLiteral && !fi.IsInitOnly).ToList();
+        
+    }
+}
